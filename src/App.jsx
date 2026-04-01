872a4bee9c9bf075a50f93c0db53dcd49f805500
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-04-01 21:48:43 +0600

    learn app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-       
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
+      <Person></Person>
      
+      <Stack></Stack>
+    </>
+  )
+}
+
+function Person() {
+  return (
+    <h1>WebDev Sakil</h1>
+  )
+}
+
+
+function Stack(){
+  return(
+    <>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>JavaScript</li>
+    <li>React</li>
+    <li>Tailwind</li>
+    <li>DaisyUi</li>
     </>
   )
 }
